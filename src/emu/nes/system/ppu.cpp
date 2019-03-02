17c02f6c91c89618255ef9697cdc19e190975b75
Forrest Jones <fjones-dev@8bitforest.com>
2019-03-02 16:33:11 -0700

    :sparkles: Move ppu flags to their own variables

--- src/emu/nes/system/ppu.cpp
+++ src/emu/nes/system/ppu.cpp
@@ -22,9 +22,38 @@ void ppu::start() {
     mem->set_ppu_reg_handler(
             std::bind(&ppu::reg_handler, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
 
-    v = 0;
-    t = 0;
-    x = 0;
+    ctrl_base_nametable = 0;
+    ctrl_vram_increment = 0;
+    ctrl_sprite_table = 0;
+    ctrl_bg_table = 0;
+    ctrl_sprite_size = 0;
+    ctrl_master_slave = 0;
+    ctrl_gen_nmi = 0;
+
+    mask_grayscale = 0;
+    mask_bg_left_col = 0;
+    mask_sprite_left_col = 0;
+    mask_bg_enable = 0;
+    mask_sprite_enable = 0;
+    mask_emphasize_red = 0;
+    mask_emphasize_green = 0;
+    mask_emphasize_blue = 0;
+
+    status_sprite_overflow = 0;
+    status_sprite_0_hit = 0;
+    status_vblank = 0;
+
+    v_coarse_x = 0;
+    v_coarse_y = 0;
+    v_nametable = 0;
+    v_fine_y = 0;
+
+    t_coarse_x = 0;
+    t_coarse_y = 0;
+    t_nametable = 0;
+    t_fine_y = 0;
+
+    fine_x = 0;
     w = false;
 
     start_time = std::chrono::high_resolution_clock::now();
@@ -67,7 +96,12 @@ void ppu::cycle() {
 
 void ppu::frame() {
     // TODO:  Prerender scanline
-    v = 0;
+
+
+    v_coarse_x = t_coarse_x;
+    v_coarse_y = t_coarse_y;
+    v_nametable = t_nametable;
+    v_fine_y = t_fine_y;
     for (int i = 0; i < 239; i++) {
         scanline(i);
         inc_v_y();
@@ -80,17 +114,14 @@ void ppu::scanline(int line_num) {
 
     // TODO: Split into cycles
     for (int i = 0; i < 256 / 8; i++) {
-        int coarse_x = v & 0x001f;
-        int coarse_y = (v & 0x03e0) >> 5;
-        int fine_y = v >> 12;
+        int attr_x = (v_coarse_x % 4) / 2;
+        int attr_y = (v_coarse_y % 4) / 2;
 
-        int attr_x = (coarse_x % 4) / 2;
-        int attr_y = (coarse_y % 4) / 2;
+        uint8_t pattern = mem->get_ppu((uint16_t) (0x2000 | (v_nametable << 10) | (v_coarse_y << 5) | v_coarse_x));
+        uint8_t attribute =
+                mem->get_ppu((uint16_t) (0x23c0 | (v_nametable << 10) | ((v_coarse_y >> 2) << 3) | (v_coarse_x >> 2)));
 
-        uint8_t pattern = mem->get_ppu((uint16_t) (0x2000 | (v & 0x0fff)));
-        uint8_t attribute = mem->get_ppu((uint16_t) (0x23c0 | (v & 0x0c00) | ((v >> 4) & 0x38) | ((v >> 2) & 0x07)));
-
-        uint16_t pattern_addr = (uint16_t) ((pattern << 4) | fine_y | ((*ppu_ctrl & 0x10) << 8));
+        uint16_t pattern_addr = (uint16_t) ((pattern << 4) | v_fine_y | ((*ppu_ctrl & 0x10) << 8));
         uint8_t pattern_lo = mem->get_ppu(pattern_addr);
         uint8_t pattern_hi = mem->get_ppu((uint16_t) (pattern_addr | 0x08));
 
@@ -98,8 +129,9 @@ void ppu::scanline(int line_num) {
 
         uint8_t palette = (uint8_t) ((attribute >> (attr_y * 4 + attr_x * 2)) & 0x03);
 
-        for (int fine_x = 0; fine_x < 8; fine_x++) {
-            uint8_t pixel = (uint8_t) (((pattern_hi >> (7 - fine_x) & 0x01) << 1) | ((pattern_lo >> (7 - fine_x)) & 0x01));
+        for (int j = 0; j < 8; j++) {
+            uint8_t pixel = (uint8_t) (((pattern_hi >> (7 - fine_x) & 0x01) << 1) |
+                                       ((pattern_lo >> (7 - fine_x)) & 0x01));
 
             uint8_t color;
             if (pixel == 0) {
@@ -108,93 +140,153 @@ void ppu::scanline(int line_num) {
                 color = mem->get_ppu((uint16_t) (0x3f00 + palette * 4 + pixel));
             }
 
-            display->set_pixel(coarse_x * 8 + fine_x, coarse_y * 8 + fine_y, color);
+            display->set_pixel(v_coarse_x * 8 + fine_x, v_coarse_y * 8 + v_fine_y, color);
+
+            fine_x++;
+            fine_x &= 0x07;
         }
 
         inc_v_x();
 //        uint8_t attribute_table = //
 //        uint16_t tile_bitmap = ...;
     }
-
-
-}
-
-void ppu::inc_v() {
-    if ((*ppu_ctrl & 0x04) == 0x04)
-        inc_v_y();
-    else
-        inc_v_x();
 }
 
 void ppu::inc_v_x() {
-    if ((v & 0x001f) == 31) {
-        v &= 0xffe0;
-        v ^= 0x0400;
+    if (v_coarse_x == 31) {
+        v_coarse_x = 0;
+        v_nametable ^= 0x1;
     } else {
-        v++;
+        v_coarse_x++;
     }
 }
 
 void ppu::inc_v_y() {
-    if ((v & 0x7000) != 0x7000) {
-        v += 0x1000;
+    if (v_fine_y < 7) {
+        v_fine_y++;
     } else {
-        v &= ~0x7000;
-        int y = (v & 0x03e0) >> 5;
-        if (y == 29) {
-            y = 0;
-            v ^= 0x0800;
-        } else if (y == 31) {
-            y = 0;
+        v_fine_y = 0;
+        if (v_coarse_y == 29) {
+            v_coarse_y = 0;
+            v_nametable ^= 0x2;
+        } else if (v_coarse_y == 31) {
+            v_coarse_y = 0;
         } else {
-            y += 1;
+            v_coarse_y++;
         }
-        v = (uint16_t) ((v & ~0x03e0) | (y << 5));
     }
 }
 
 void ppu::reg_handler(uint8_t &value, uint8_t new_value, bool write) {
-    // TODO: the rest of them
     if (&value == ppu_ctrl && write) {
-        t = (uint8_t) ((t & 0x73ff) | ((new_value & 0x03) << 10));
+        set_ctrl(new_value);
+    } else if (&value == ppu_mask && write) {
+        set_mask(new_value);
     } else if (&value == ppu_status && !write) {
-        w = false;
-    } else if (&value == oam_data && write) { // TODO: && !rendering
-        // TODO: oam_data read
+        value = get_status();
+    } else if (&value == oam_data && write && !rendering) {
         mem->set_oam(*oam_addr, new_value);
         (*oam_addr)++;
-    } else if (&value == ppu_addr && write) {
+    } else if (&value == oam_addr && !write) {
+        value = mem->get_oam(*oam_addr);
+    } else if (&value == ppu_scroll && write) {
         if (!w) {
-            t = (uint16_t) (((new_value & 0x3f) << 8) | (t & 0xff)); // Upper byte
+            t_coarse_x = new_value >> 3;
+            fine_x = new_value & (uint8_t) 0x07;
         } else {
-            t = (uint16_t) (new_value | (t & 0xff00)); // Lower byte
-            v = t;
+            t_coarse_y = new_value >> 3;
+            t_fine_y = new_value & (uint8_t) 0x07;
         }
         w = !w;
-    } else if (&value == ppu_data && write) { // TODO: acts weird during rendering
-        mem->set_ppu(v, new_value);
-        if ((*ppu_ctrl & 0x04) == 0x04) {
-            v += 32;
+    } else if (&value == ppu_addr && write) {
+        if (!w) {
+            t_coarse_y = (uint8_t) ((t_coarse_y & 0x07) | ((new_value & 0x03) << 3));
+            t_nametable = (new_value >> 2) & (uint8_t) 0x03;
+            t_fine_y = (new_value >> 4) & (uint8_t) 0x03;
         } else {
-            v++;
+            t_coarse_x = new_value & (uint8_t) 0x1f;
+            t_coarse_y = (uint8_t) ((t_coarse_y & 0x18) | (new_value >> 5));
+
+            v_coarse_x = t_coarse_x;
+            v_coarse_y = t_coarse_y;
+            v_nametable = t_nametable;
+            v_fine_y = t_fine_y;
         }
-    } else if (&value == ppu_data && !write) {
-        value = mem->get_ppu(v);
-        if ((*ppu_ctrl & 0x04) == 0x04) {
-            v += 32;
+        w = !w;
+    } else if (&value == ppu_data) {
+        if (write)
+            mem->set_ppu(get_v(), new_value);
+        else
+            value = mem->get_ppu(get_v());
+
+        if (status_vblank || !rendering) {
+            if (ctrl_vram_increment) {
+                set_v((uint16_t) (get_v() + 32));
+            } else {
+                set_v((uint16_t) (get_v() + 1));
+            }
         } else {
-            v++;
+            inc_v_x();
+            inc_v_y();
         }
-    } else if (&value == ppu_status && !write) {
-        // TODO: Actually return the status
-        value = 0x80;
     }
+}
+
+void ppu::set_ctrl(uint8_t ctrl) {
+    ctrl_base_nametable = ctrl & (uint8_t) 0x03;
+    ctrl_vram_increment = (ctrl >> 2) & (uint8_t) 0x01;
+    ctrl_sprite_table = (ctrl >> 3) & (uint8_t) 0x01;
+    ctrl_bg_table = (ctrl >> 4) & (uint8_t) 0x01;
+    ctrl_sprite_size = (ctrl >> 5) & (uint8_t) 0x01;
+    ctrl_master_slave = (ctrl >> 6) & (uint8_t) 0x01;
+    ctrl_gen_nmi = (ctrl >> 7) & (uint8_t) 0x01;
+    if (ctrl_gen_nmi && status_vblank)
+        interrupt_handler->set_nmi();
+
+    t_nametable = ctrl_base_nametable;
+}
+
+void ppu::set_mask(uint8_t mask) {
+    mask_grayscale = mask & (uint8_t) 0x01;
+    mask_bg_left_col = (mask >> 1) & (uint8_t) 0x01;
+    mask_sprite_left_col = (mask >> 2) & (uint8_t) 0x01;
+    mask_bg_enable = (mask >> 3) & (uint8_t) 0x01;
+    mask_sprite_enable = (mask >> 4) & (uint8_t) 0x01;
+    mask_emphasize_red = (mask >> 5) & (uint8_t) 0x01;
+    mask_emphasize_green = (mask >> 6) & (uint8_t) 0x01;
+    mask_emphasize_blue = (mask >> 7) & (uint8_t) 0x01;
+}
+
+uint8_t ppu::get_status() {
+    uint8_t status = 0;
+    status |= status_sprite_overflow << 5;
+    status |= status_sprite_0_hit << 6;
+    status |= status_vblank << 7;
+    status_vblank = 0;
+    w = false;
+
+    return status;
+}
+
+void ppu::set_v(uint16_t v) {
+    v_coarse_x = (uint8_t) (v & 0x1f);
+    v_coarse_y = (uint8_t) ((v >> 5) & 0x1f);
+    v_nametable = (uint8_t) ((v >> 10) & 0x03);
+    v_fine_y = (uint8_t) ((v >> 12) & 0x07);
+}
+
+uint16_t ppu::get_v() {
+    uint16_t v = 0;
+    v |= v_coarse_x;
+    v |= v_coarse_y << 5;
+    v |= v_nametable << 10;
+    v |= v_fine_y << 12;
+
+    return v;
 }
 
 void ppu::setup_palette() {
     int palette[64];
     nes_2C02_palette().get_palette(palette);
-
-    for (int i = 0; i < 64; i++)
-        display->set_palette(palette, 64);
+    display->set_palette(palette, 64);
 }
--- src/emu/nes/system/ppu.h
+++ src/emu/nes/system/ppu.h
@@ -16,7 +16,7 @@ class ppu {
     std::unique_ptr<spdlog::logger> log = utils::get_logger("ppu");
 
     ::mem *mem;
-	emu_display *display;
+    emu_display *display;
     ::interrupt_handler *interrupt_handler;
 
     uint64_t current_cycle;
@@ -33,13 +33,50 @@ class ppu {
     uint8_t *ppu_addr;
     uint8_t *ppu_data;
 
-    uint16_t v;
-    uint16_t t;
-    uint8_t x;
+    // PPU CTRL
+    uint8_t ctrl_base_nametable;
+    uint8_t ctrl_vram_increment;
+    uint8_t ctrl_sprite_table;
+    uint8_t ctrl_bg_table;
+    uint8_t ctrl_sprite_size;
+    uint8_t ctrl_master_slave;
+    uint8_t ctrl_gen_nmi;
+
+    // PPU MASK
+    uint8_t mask_grayscale;
+    uint8_t mask_bg_left_col;
+    uint8_t mask_sprite_left_col;
+    uint8_t mask_bg_enable;
+    uint8_t mask_sprite_enable;
+    uint8_t mask_emphasize_red;
+    uint8_t mask_emphasize_green;
+    uint8_t mask_emphasize_blue;
+
+    // PPU STATUS
+    uint8_t status_sprite_overflow;
+    uint8_t status_sprite_0_hit;
+    uint8_t status_vblank;
+
+    // V
+    uint8_t v_coarse_x;
+    uint8_t v_coarse_y;
+    uint8_t v_nametable;
+    uint8_t v_fine_y;
+
+    // T
+    uint8_t t_coarse_x;
+    uint8_t t_coarse_y;
+    uint8_t t_nametable;
+    uint8_t t_fine_y;
+
+    // State
+    bool rendering;
+
+    uint8_t fine_x;
     bool w;
 
 public:
-    explicit ppu(::mem *mem, emu_display* display);
+    explicit ppu(::mem *mem, emu_display *display);
 
     void start();
 
@@ -55,15 +92,23 @@ private:
 
     void scanline(int line_num);
 
-    void inc_v();
-
     void inc_v_x();
 
     void inc_v_y();
 
     void reg_handler(uint8_t &value, uint8_t new_value, bool write);
 
-	void setup_palette();
+    void set_ctrl(uint8_t ctrl);
+
+    void set_mask(uint8_t mask);
+
+    uint8_t get_status();
+
+    void set_v(uint16_t v);
+
+    uint16_t get_v();
+
+    void setup_palette();
 };
 
 #endif //TERMU_PPU_H
